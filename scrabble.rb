580848a68a4dbda5f36291c5aac3e137b1a03a57
hoodq <hoodq@colorado.edu>
2015-04-03 11:28:54 -0600

    made the hash an instance variable to be used in future methods

--- scrabble.rb
+++ scrabble.rb
@@ -5,18 +5,18 @@ class ScrabbleWord
 
 	def initialize(word)
 		@word = word.downcase
-	end
-
-	def score
-		letter_points = {'a' => 1, 'b' => 3, 'c' => 3, 'd' => 2, 'e' => 1, 'f' => 4,
+		@letter_points = {'a' => 1, 'b' => 3, 'c' => 3, 'd' => 2, 'e' => 1, 'f' => 4,
 				 'g' => 2, 'h' => 4, 'i' => 1, 'j' => 8, 'k' => 5, 'l' => 1,
 				 'm' => 3, 'n' => 1, 'o' => 1, 'p' => 3, 'q' => 10, 'r' => 1,
 				 's' => 1, 't' => 1, 'u' => 1, 'v' => 4, 'w' => 4, 'x' => 8,
 				 'y' => 4, 'z' => 10}
+	end
+
+	def score
 		total = 0
 		
 		@word.each_char do |letter|
-			total += letter_points[letter]
+			total += @letter_points[letter]
 		end
 		total
 	end
