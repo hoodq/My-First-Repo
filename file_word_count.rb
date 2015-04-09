dbbad5d511bc5b1959a1fd6f1dbd7b3281e4782e
hoodq <hoodq@colorado.edu>
2015-04-09 16:39:30 -0600

    added word input functionality

--- file_word_count.rb
+++ file_word_count.rb
@@ -1,4 +1,5 @@
 #reads a file and stores each word (set to lowercase) used and the number of times that word is used in a hash
+#then it prompts the user for a word to search and then says the number of times that word appears in the file
 
 def word_count_tracker
   a_file = File.open('words.txt')
@@ -10,13 +11,14 @@ def word_count_tracker
   	end
   end
   a_file.close
-  words.each do |word, count|
-    puts "#{word}: #{count}"
-  end
+  puts "please enter the desired word"
+  word = gets.chomp
+  puts "#{word} appears #{words[word]} time(s)"
 end
 
 def line_to_array line
   line.split(/[^\w+$]/)
 end
+
 
 word_count_tracker
